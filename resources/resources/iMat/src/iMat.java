068112e565ffd00aa8c6cb96368fdc526187d05c
silarss <zico.xp.larsson@gmail.com>
2020-05-13 21:20:56 +0200

    testar

--- resources/resources/iMat/src/iMat.java
+++ resources/resources/iMat/src/iMat.java
@@ -1,3 +1,5 @@
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
 public class iMat {
     public static void main(String[] args) {
         System.out.println("testar");
@@ -5,6 +7,11 @@ public class iMat {
         System.out.println("12345");
         System.out.println("hej");
         System.out.println("Pool");
+        initialize();
 
+    }
+    private static void initialize(){
+        IMatDataHandler datahandler = IMatDataHandler.getInstance();
+        //anagligen mer som ska skapas
     }
 }
