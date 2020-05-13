6baed588613293990f2765624ba6cb081e2fcdd7
toblinds <toblinds@student.chalmers.se>
2020-05-13 16:28:53 +0200

    h

--- resources/resources/iMat/src/iMat.java
+++ resources/resources/iMat/src/iMat.java
@@ -2,5 +2,6 @@ public class iMat {
     public static void main(String[] args) {
         System.out.println("testar");
         System.out.println("lol");
+        System.out.println("12345");
     }
 }
