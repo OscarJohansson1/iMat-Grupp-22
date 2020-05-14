0ccfaf90355c6a542ce30678b92a9ff5050f7b51
toblinds <toblinds@student.chalmers.se>
2020-05-14 11:04:32 +0200

    h

--- resources/resources/iMat/src/iMat.java
+++ resources/resources/iMat/src/iMat.java
@@ -1,13 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
-public class iMat {
+import java.util.ResourceBundle;
+
+public class iMat //extends Application
+ {
+
+    /*@Override
+    public void start(Stage stage) throws Exception {
+
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("iMat");
+
+        Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"), bundle);
+
+        Scene scene = new Scene(root, 800, 500);
+
+        stage.setTitle(bundle.getString("application.name"));
+        stage.setScene(scene);
+        stage.show();
+
+    } */
     public static void main(String[] args) {
-        System.out.println("testar");
-        System.out.println("lol");
-        System.out.println("12345");
-        System.out.println("hej");
-        System.out.println("Pool");
         initialize();
+        launch(args);
+
     }
     private static void initialize(){
         IMatDataHandler datahandler = IMatDataHandler.getInstance();
