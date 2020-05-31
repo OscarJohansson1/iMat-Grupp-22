f01b78a07d15bda3d5289bf48c9782236dd5c9a5
Oscar <oscarjohansson999@gmail.com>
2020-05-31 20:14:20 +0200

    qol

--- resources/resources/iMat/src/myHistoryController.java
+++ resources/resources/iMat/src/myHistoryController.java
@@ -1,5 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
@@ -15,6 +16,7 @@ public class myHistoryController extends AnchorPane {
     @FXML private FlowPane flowPane2;
     @FXML private AnchorPane fristPane;
     @FXML private AnchorPane secondPane;
+    @FXML private Button backButtonMyHistory;
 
     public myHistoryController(Controller c){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("myHistory.fxml"));
@@ -31,11 +33,15 @@ public class myHistoryController extends AnchorPane {
         this.controller = c;
 
         fristPane.toFront();
+        backButtonMyHistory.setOnMouseClicked(m->{
+            fristPane.toFront();
+        });
 
         addOrders();
     }
 
     private void addOrders() {
+        flowPane1.getChildren().clear();
         for (Order o : datahandler.getOrders()){
             myHistoryDetailsController m = new myHistoryDetailsController(controller, o, this);
             flowPane1.getChildren().add(m);
@@ -45,6 +51,7 @@ public class myHistoryController extends AnchorPane {
     public void showSecondPane(Order order) {
         secondPane.toFront();
         fristPane.toBack();
+        flowPane2.getChildren().clear();
         for (ShoppingItem si : order.getItems()){
             myHistoryDetailsDetails mhdd = new myHistoryDetailsDetails(controller, si);
             flowPane2.getChildren().add(mhdd);
