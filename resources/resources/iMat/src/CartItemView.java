2ee6000e8af4ed59c2fb3406762f2438deede1a1
Oscar <oscarjohansson999@gmail.com>
2020-05-31 13:12:25 +0200

    Halfdone, but working kundvagnsedit

--- resources/resources/iMat/src/CartItemView.java
+++ resources/resources/iMat/src/CartItemView.java
@@ -1,6 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.event.Event; import javafx.fxml.FXML; import javafx.fxml.FXMLLoader; import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -23,10 +24,23 @@ public class CartItemView extends AnchorPane {
     Label cartTotalItemPrize;
     @FXML
     ImageView cartEditPen;
+    @FXML
+    AnchorPane cartItemsPane;
+    BigItemView biv;
 
     //private final Product product;
     private final ShoppingItem shoppingItem;
     private final IMatDataHandler datahandler;
+
+    ImageView plus = new ImageView(new Image(getClass().getResource("/sceneImages/baseline_add_circle_outline_black_18dp.png").toString()));
+    ImageView minus = new ImageView(new Image(getClass().getResource("/sceneImages/baseline_remove_circle_outline_black_18dp.png").toString()));
+    ImageView delete = new ImageView(new Image(getClass().getResource("/sceneImages/baseline_delete_black_24dp.png").toString()));
+    Label amount;
+
+
+    boolean open = false;
+
+
 
     //private Controller controller;
     public CartItemView(ShoppingItem shoppingItem) {
@@ -46,6 +60,59 @@ public class CartItemView extends AnchorPane {
         this.cartItemImage.setImage(datahandler.getFXImage(shoppingItem.getProduct()));
         this.cartItemLabel.setText(shoppingItem.getProduct().getName());
         this.cartItemPerKilo.setText(Math.round(shoppingItem.getAmount()) + " " + shoppingItem.getProduct().getUnitSuffix());
+
+        amount = new Label(Integer.toString((int) shoppingItem.getAmount()));
+        biv = new BigItemView(datahandler, shoppingItem.getProduct());
+
+        addListeners();
     }
 
+    private void setUp() {
+        cartItemsPane.getChildren().add(plus);
+        cartItemsPane.getChildren().add(minus);
+        cartItemsPane.getChildren().add(delete);
+        cartItemsPane.getChildren().add(amount);
+        minus.setX(10);
+        minus.setY(55);
+        plus.setX(100);
+        plus.setY(55);
+        amount.relocate(70, 65);
+        delete.setX(170);
+        delete.setY(50);
+        cartItemsPane.setPrefHeight(100);
+    }
+
+    private void setDown() {
+        cartItemsPane.getChildren().remove(plus);
+        cartItemsPane.getChildren().remove(minus);
+        cartItemsPane.getChildren().remove(delete);
+        cartItemsPane.getChildren().remove(amount);
+        cartItemsPane.setPrefHeight(50);
+    }
+
+    private void addListeners() {
+        cartEditPen.setOnMouseClicked(m->{
+            //minusItemImage.setImage(new Image(getClass().getResource("/sceneImages/baseline_remove_circle_outline_black_18dp.png").toString()));
+            if(!open){
+                setUp();
+                open = true;
+            } else {
+                setDown();
+                open = false;
+            }
+        });
+
+        plus.setOnMouseClicked(m->{
+            biv.plusButtonActionPerformed(m);
+            amount.setText(Integer.toString((int) shoppingItem.getAmount()));
+        });
+        minus.setOnMouseClicked(m->{
+            biv.minusButtonActionPerformed(m);
+            amount.setText(Integer.toString((int) shoppingItem.getAmount()));
+        });
+        delete.setOnMouseClicked(m->{
+            datahandler.getShoppingCart().removeItem(shoppingItem);
+        });
+    }
 }
+
