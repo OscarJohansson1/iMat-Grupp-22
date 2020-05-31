0b14b04e24b42bbb598d6f74bcdb00406b627c1c
Oscar <oscarjohansson999@gmail.com>
2020-05-31 23:20:00 +0200

    mina uppgifter

--- resources/resources/iMat/src/myDetailsController.java
+++ resources/resources/iMat/src/myDetailsController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Customer;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 import java.io.IOException;
@@ -36,10 +37,20 @@ public class myDetailsController extends AnchorPane {
         this.controller = c;
         datahandler = IMatDataHandler.getInstance();
 
+        myDetailsName.setText(datahandler.getCustomer().getFirstName() + " " + datahandler.getCustomer().getLastName());
+        myDetalisAddress.setText(datahandler.getCustomer().getAddress());
+        myDetalisEmail.setText(datahandler.getCustomer().getEmail());
+        myDetailsCard.setText(datahandler.getCreditCard().getCardNumber());
+        myDetailsDate.setText(datahandler.getCreditCard().getValidMonth() + "/" + datahandler.getCreditCard().getValidYear());
+        myDetailsDate.setEditable(false);
+        myDetailsDate.setStyle("-fx-fill-color: GREY;");
+        myDetailsCVC.setEditable(false);
+        myDetailsCVC.setStyle("-fx-fill-color: GREY;");
+
         ToggleGroup cardToggleGroup = new ToggleGroup();
         myDetailsMastercard.setToggleGroup(cardToggleGroup);
         myDetailsVisa.setToggleGroup(cardToggleGroup);
-        if(datahandler.getCreditCard().getCardType() == "Visa") {
+        if(datahandler.getCreditCard().getCardType().equals("Visa")) {
             myDetailsVisa.setSelected(true);
         } else {
             myDetailsMastercard.setSelected(true);
@@ -57,6 +68,26 @@ public class myDetailsController extends AnchorPane {
                     }
                 }
             }
+        });
+
+        myDetailsName.textProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue.contains(" ")){
+                int i = newValue.indexOf(" ");
+                datahandler.getCustomer().setFirstName(newValue.substring(0, i));
+                datahandler.getCustomer().setLastName(newValue.substring(i + 1));
+            } else {
+                datahandler.getCustomer().setFirstName(newValue);
+            }
+
+        });
+        myDetalisAddress.textProperty().addListener((observable, oldValue, newValue) -> {
+            datahandler.getCustomer().setAddress(newValue);
+        });
+        myDetalisEmail.textProperty().addListener((observable, oldValue, newValue) -> {
+            datahandler.getCustomer().setEmail(newValue);
+        });
+        myDetailsCard.textProperty().addListener((observable, oldValue, newValue) -> {
+            datahandler.getCreditCard().setCardNumber(newValue);
         });
     }
 }
